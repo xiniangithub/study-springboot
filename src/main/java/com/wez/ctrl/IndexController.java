2f330f16f93377f274dc7d39ef8f1b7f66ba5da4
昔小年 <2779207210@qq.com>
2021-03-01 22:05:22 +0800

    加载配置文件

--- src/main/java/com/wez/ctrl/IndexController.java
+++ src/main/java/com/wez/ctrl/IndexController.java
@@ -1,5 +1,7 @@
 package com.wez.ctrl;
 
+import com.wez.config.DBProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/index")
 public class IndexController {
 
+    @Autowired
+    private DBProperties dbProperties;
+
     @GetMapping(value="/hello")
     public String home() {
         return "Hello World!";
+    }
+
+    @GetMapping(value="/get_db_config")
+    public DBProperties getDbConfig() {
+        return dbProperties;
     }
 
 }
